6f21c1e97d88aaef1203b273f2312255aa578ae3
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2023-01-15 16:23:19 +0200

    added signin component and firebase utils and brought in firebase library

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,14 @@
-const SignIn = ()=>{
-    return(
-        <div><h1>Sign in</h1></div>
-    )
-}
+import { signInWithGooglePopup  , createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    let {user} = await signInWithGooglePopup();
+   const userDocRef = await createUserDocumentFromAuth(user)
+  };
+  return (
+    <div>
+      <h1>Sign in</h1>
+      <button onClick={logGoogleUser}>Sign in with google popup</button>
+    </div>
+  );
+};
 export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth , GoogleAuthProvider , signInWithPopup , signInWithRedirect} from "firebase/auth";
+import {getFirestore , doc , getDoc , setDoc} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcnOx6xJEi_Wb23OOhTpVl4WY61ouoFHU",
+  authDomain: "crwn-clothing-db-78a2c.firebaseapp.com",
+  projectId: "crwn-clothing-db-78a2c",
+  storageBucket: "crwn-clothing-db-78a2c.appspot.com",
+  messagingSenderId: "74113490878",
+  appId: "1:74113490878:web:bceebe74ab2dfb30d9a3ef"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt:"select_account"
+})
+export const auth = getAuth()
+export const signInWithGooglePopup=()=>signInWithPopup(auth , provider);
+/* db variable points to database in the firebase console */
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth)=>{
+    const userDocRef = doc(db , 'users' , userAuth.uid )
+    console.log(userDocRef);
+    /*allows us to access the data or check wether the data exists or  not */
+    const userSnapShot = await getDoc(userDocRef);
+    console.log(userSnapShot.exists())
+    if(!userSnapShot.exists()){
+      const {displayName , email} = userAuth;
+      const createdAt = new Date()
+      try{
+        await setDoc(userDocRef , {
+          displayName,
+          email,
+          createdAt
+        })
+      }catch(err){
+        console.log("user creating error ", err.message)
+      }
+      return userDocRef;
+    }
+}
